2d52f67a079c6cdf88d99c381574a3c82425ae3a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-12-24 23:15:08 +0800

    init wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import path from "path"
+
+import { defineConfig } from "vite"
+
+import react from "@vitejs/plugin-react"
+
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./www/components")
