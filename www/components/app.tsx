fffa06d0af568301c0fdcae9286933c04c42b2e2
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-12-24 23:39:27 +0800

    init hono

--- www/components/app.tsx
+++ www/components/app.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react"
+
 export function App() {
-  return <h1>App</h1>
+  const [message, setMessage] = useState({})
+
+  useEffect(() => {
+    async function getMessage() {
+      const request = await fetch("/api/status")
+
+      if (!request.ok) {
+        return setMessage({ message: "not okay :(" })
+      }
+
+      const response = await request.json()
+      setMessage(response)
+    }
+
+    getMessage()
+  }, [])
+
+  return (
+    <div>
+      <h1>Message from Hono</h1>
+      <pre>{JSON.stringify(message, null, 2)}</pre>
+    </div>
+  )
 }
