1c51086784f319b1046b55a53733c98eeb3e2499
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-12-24 23:39:27 +0800

    init hono

--- workers/index.ts
+++ workers/index.ts
@@ -1,5 +1,14 @@
-export default {
-  fetch() {
-    return new Response(`Running in ${navigator.userAgent}!`)
-  }
-}
+import { Hono } from "hono"
+import { secureHeaders } from "hono/secure-headers"
+
+const app = new Hono({ strict: true }).basePath("/api")
+
+app.use(secureHeaders())
+
+app.get("/status", ctx => {
+  return ctx.json({
+    status: "healthy"
+  })
+})
+
+export default app
